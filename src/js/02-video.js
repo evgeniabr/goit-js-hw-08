0854e4343270bba162b900d45e86b7ae12ade62f
evgeniabr <113677474+evgeniabr@users.noreply.github.com>
2023-03-05 02:12:34 +0200

    ***

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+ const iframe = document.querySelector('iframe');
+    const player = new Player(iframe);
+const LOKALSTORAGE_KEY = "videoplayer-current-time";
+
+const onPlay = function(data) {
+    localStorage.setItem(LOKALSTORAGE_KEY, data.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const storedTime = localStorage.getItem(LOKALSTORAGE_KEY);
+
+if (storedTime) {
+    player.setCurrentTime(storedTime);
+}
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from "lodash.throttle";
 
+const feedbackForm = document.querySelector('.feedback-form');
+const feedbackMessage = document.querySelector('textarea');
+const feedBackEmail = document.querySelector('input');
+
+const STORAGE_KEY = "feedback-form-state";
+
+
+
+feedbackForm.addEventListener('submit', onFeedbackFormSubmit);
+feedbackForm.addEventListener('input', throttle(onFeedbackInput, 500));
+
+populateFeedbackInfo();
+
+function onFeedbackFormSubmit(event) {
+    event.preventDefault();
+    event.currentTarget.reset();
+     console.log(JSON.parse(localStorage.getItem(STORAGE_KEY)));
+    localStorage.removeItem(STORAGE_KEY);
+}
+
+function onFeedbackInput() {
+    const feedBackInfo = {
+        email: feedBackEmail.value,
+        message: feedbackMessage.value,
+    };
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(feedBackInfo));
+}
+
+function populateFeedbackInfo() {
+    const savedFeedbackInfo = localStorage.getItem(STORAGE_KEY);
+    if (savedFeedbackInfo) {
+        feedBackEmail.value = JSON.parse(savedFeedbackInfo).email;
+        feedbackMessage.value = JSON.parse(savedFeedbackInfo).message;
+    } else {
+        feedBackEmail.value = "";
+        feedbackMessage.value = "";
+        
+    }
+}
